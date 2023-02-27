184cbe3811da63da3dbe5ba9be87f0fb062d1e57
mohameddekow <mohameddekow77@gmail.com>
2023-02-27 10:37:52 +0300

    package structure

--- app/src/main/java/com/example/odsas/deana_module/presentation/PLACEHOLDERFILE.kt
+++ app/src/main/java/com/example/odsas/deana_module/presentation/PLACEHOLDERFILE.kt
@@ -1,3 +1,3 @@
-package com.example.odsas.deana_module.data
+package com.example.odsas.deana_module.presentation
 
 //@TODO JUST A PLACE HOLDER FILE TO MAINTAIN PACKAGE STRUCTURE u can delete any time you have content for this class
